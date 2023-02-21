b35c3a8e37bd9f4495fe4588c0cf5f717c3b653f
davidmcdermidsnowflake <126002463+davidmcdermidsnowflake@users.noreply.github.com>
2023-02-21 15:28:32 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,7 +12,7 @@ def normalize(fruit_choice):
 def insert_row_snowflake(new_fruit):
     my_cnx = snowflake.connector.connect(**st.secrets["snowflake"])   
     with my_cnx.cursor() as my_cur:
-        my_cur.execute("insert into fruit_load_list values ('" + new_fruit "')")
+        my_cur.execute("insert into fruit_load_list values ('" + new_fruit + "')")
         my_cnx.close()
         return "Thanks for adding " + new_fruit
     
